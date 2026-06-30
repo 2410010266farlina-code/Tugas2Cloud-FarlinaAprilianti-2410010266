b28921da54d78961caedfaf28e7a364641a4531d
2410010266farlina-code <2410010266farlina@gmail.com>
2026-06-30 22:44:18 +0800

    Update tampilan waktu di web

--- server.js
+++ server.js
@@ -3,7 +3,21 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send('<h1>Halo, Tugas 2 Cloud Berhasil Dijalankan di Docker!</h1><p>Nama: Farlina APrilianti<br>NPM: 2410010266</p>');
+    // Mengambil waktu dan tanggal saat ini sesuai zona waktu lokal komputer
+    const waktuSekarang = new Date().toLocaleString('id-ID', {
+        dateStyle: 'full',
+        timeStyle: 'medium'
+    });
+
+    res.send(`
+        <div style="font-family: Arial, sans-serif; text-align: center; margin-top: 50px;">
+            <h1>Halo, Tugas 2 Cloud Berhasil Dijalankan di Docker!</h1>
+            <p>Nama: Farlina Aprilianti<br>NPM: 2410010266</p>
+            <hr style="width: 50%; margin: 20px auto;">
+            <h3 style="color: #2c3e50;">Waktu Akses Browser:</h3>
+            <p style="font-size: 1.2rem; font-weight: bold; color: #e74c3c;">${waktuSekarang}</p>
+        </div>
+    `);
 });
 
 app.listen(PORT, () => {
